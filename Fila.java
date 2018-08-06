563631d1600315224952d90a5f5d2fe1293e65c9
Amanda Pereira <amanda.souza@dce.ufpb.br>
2018-08-06 15:28:37 -0300

    Atualização da classe

--- Fila.java
+++ Fila.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 public class Fila {
 	private ArrayList<Paciente> pacientes;
 	
-	public Fila(ArrayList<Paciente> pacientes) {
-		this.pacientes = pacientes;
+	public Fila() {
+		this.pacientes = new ArrayList<>();
 		
 	}
 	
@@ -16,11 +16,10 @@ public class Fila {
 		this.pacientes = pacientes;
 	}
 	
-	public void addPacienteNaFila(ArrayList<Paciente> pacientes) {
-		ArrayList<Fila> fila = new ArrayList<Fila>();
+	public void addPacienteNaFila(Paciente paciente) {
 		for(Paciente p: this.pacientes) {
-			if(p != null) {
-				this.pacientes.add(fila);
+			if(p.getNome().equalsIgnoreCase(paciente.getNome())) {
+				this.pacientes.add(paciente);
 			}
 		}
 	}
